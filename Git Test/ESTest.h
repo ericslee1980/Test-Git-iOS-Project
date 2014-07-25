3ab3f718c1d76ae1726a3a5e399df0b3dca5acae
Eric Slee <ericslee1980@yahoo.com>
2014-07-25 00:28:42 -0700

    added a test class

---
+++ Git Test/ESTest.h
@@ -0,0 +1,13 @@
+//
+//  ESTest.h
+//  Git Test
+//
+//  Created by Eric Slee on 7/25/14.
+//  Copyright (c) 2014 Eric Slee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ESTest : NSObject
+
+@end
---
+++ Git Test/ESTest.m
@@ -0,0 +1,13 @@
+//
+//  ESTest.m
+//  Git Test
+//
+//  Created by Eric Slee on 7/25/14.
+//  Copyright (c) 2014 Eric Slee. All rights reserved.
+//
+
+#import "ESTest.h"
+
+@implementation ESTest
+
+@end
